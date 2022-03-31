ac24543c8f1ac0725666cfa471b3e8a655f3b1fd
JPabloMF <juanpablo45671@gmail.com>
2022-03-31 09:23:56 -0500

    Creating initial sections

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,20 @@
 import * as React from "react";
 import GlobalStyle from "../theme/globalStyle";
+import Home from "./home"
+import AboutMe from "./aboutMe"
+import Projects from "./projects"
+import Blog from "./blog"
+import Footer from "./footer"
 
 const IndexPage = () => {
   return (
     <>
       <GlobalStyle />
+      <Home />
+      <AboutMe />
+      <Projects />
+      <Blog />
+      <Footer />
     </>
   );
 };
