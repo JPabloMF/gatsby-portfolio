15bc1effd7303e5efcdfe6f76fde36f3145151f0
JPabloMF <juanpablo45671@gmail.com>
2022-04-04 15:39:51 -0500

    reusing text methods

---
+++ src/components/texts/utils/index.ts
@@ -0,0 +1,44 @@
+export interface TitleProps {
+  isGreen: boolean;
+  text?: string;
+  size?: string;
+  theme: {
+    fontSize: {
+      title: string;
+    };
+    colors: {
+      cold: {
+        blue: string;
+        lightBlue: string;
+        green: string;
+      };
+      warm: {
+        purple: string;
+        pink: string;
+        orange: string;
+      };
+    };
+  };
+}
+
+interface IGradientColor {
+    isGreen: boolean;
+    theme: {
+        colors: {
+            cold: { blue: string; lightBlue: string; green: string; };
+            warm: { purple: string; pink: string; orange: string; };
+        }
+    };
+}
+
+export const getGradientColor = ({ isGreen, theme }: IGradientColor) => {
+  const {
+    colors: {
+      cold: { blue, lightBlue, green },
+      warm: { purple, pink, orange },
+    },
+  } = theme;
+  if (isGreen)
+    return `-webkit-linear-gradient(0deg, ${blue}, ${lightBlue}, ${green});`;
+  return `-webkit-linear-gradient(0deg, ${purple}, ${pink}, ${orange});`;
+};
