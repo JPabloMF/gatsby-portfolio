4cd468dcc7b298246fdd745cdb952326a624e94e
JPabloMF <juanpablo45671@gmail.com>
2022-04-06 12:53:28 -0500

    Add theme fontweight and tech icon colors

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -5,6 +5,10 @@ const theme = {
     text: "1.5rem",
     icons: "4rem",
   },
+  fontWeight: {
+    bold: "bold",
+    initial: "initial",
+  },
   colors: {
     cold: {
       blue: "#386ae8",
@@ -15,6 +19,15 @@ const theme = {
       purple: "#603b9b",
       pink: "#c1429b",
       orange: "#ff9571",
+    },
+    tech: {
+      html: "#e34c26",
+      css: "#264de3",
+      js: "#f0db4f",
+      react: "#61dafb",
+      redux: "#764abc",
+      git: "#e84e31",
+      typescript: "#2f74c0",
     },
     white: "#fff",
     gray: "#415162",
