1c3faf9fcd58efacedb294fcdb7e05e95c57772e
JPabloMF <juanpablo45671@gmail.com>
2022-04-06 12:50:09 -0500

    Add Icons component

---
+++ src/components/icons/index.ts
@@ -0,0 +1,69 @@
+import styled from "styled-components";
+
+import { FaHtml5, FaCss3Alt, FaJs, FaReact, FaGitAlt } from "react-icons/fa";
+import { SiRedux, SiTypescript } from "react-icons/si";
+
+interface IIconProps {
+  size?: string;
+  theme: {
+    fontSize: {
+      icons: string;
+    };
+    colors: {
+      tech: {
+        html: string;
+        css: string;
+        js: string;
+        git: string;
+        react: string;
+        redux: string;
+        typescript: string;
+      };
+    };
+  };
+}
+
+const StyledHTMLIcon = styled(FaHtml5)`
+  font-size: ${({ theme, size }: IIconProps) => size || theme.fontSize.icons};
+  color: ${({ theme }: IIconProps) => theme.colors.tech.html};
+`;
+
+const StyledCSSIcon = styled(FaCss3Alt)`
+  font-size: ${({ theme, size }: IIconProps) => size || theme.fontSize.icons};
+  color: ${({ theme }: IIconProps) => theme.colors.tech.css};
+`;
+
+const StyledJSIcon = styled(FaJs)`
+  font-size: ${({ theme, size }: IIconProps) => size || theme.fontSize.icons};
+  color: ${({ theme }: IIconProps) => theme.colors.tech.js};
+`;
+
+const StyledReactIcon = styled(FaReact)`
+  font-size: ${({ theme, size }: IIconProps) => size || theme.fontSize.icons};
+  color: ${({ theme }: IIconProps) => theme.colors.tech.react};
+`;
+
+const StyledGitIcon = styled(FaGitAlt)`
+  font-size: ${({ theme, size }: IIconProps) => size || theme.fontSize.icons};
+  color: ${({ theme }: IIconProps) => theme.colors.tech.git};
+`;
+
+const StyledReduxIcon = styled(SiRedux)`
+  font-size: ${({ theme, size }: IIconProps) => size || theme.fontSize.icons};
+  color: ${({ theme }: IIconProps) => theme.colors.tech.redux};
+`;
+
+const StyledTypescriptIcon = styled(SiTypescript)`
+  font-size: ${({ theme, size }: IIconProps) => size || theme.fontSize.icons};
+  color: ${({ theme }: IIconProps) => theme.colors.tech.typescript};
+`;
+
+export {
+  StyledHTMLIcon as IHTML,
+  StyledCSSIcon as ICSS,
+  StyledJSIcon as IJS,
+  StyledReactIcon as IReact,
+  StyledGitIcon as IGit,
+  StyledReduxIcon as IRedux,
+  StyledTypescriptIcon as ITypescript,
+};
